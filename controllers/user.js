edd84fe539e6d565666952ca9ba5ea63365a778d
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2022-01-09 19:32:23 -0500

    add: method to create users

---
+++ controllers/user.js
@@ -0,0 +1,13 @@
+import User from '../models/user.js';
+
+export const createUser = async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const newUser = new User({ username });
+    const userCreated = await newUser.save();
+    res.json({ _id: userCreated._id, username: userCreated.username });
+  } catch (error) {
+    res.status(400).json({ error: error.message ?? error });
+  }
+};
