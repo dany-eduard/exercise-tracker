9b52fd7081a2b097296c9342716158dfb16a3a72
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2022-01-09 22:07:06 -0500

    add: route to save an exercise

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { createUser, listUsers } from '../controllers/user.js';
+import { saveExercise } from '../controllers/exercises.js';
 const router = Router();
 
 router.get('/users', listUsers);
 router.post('/users', createUser);
+router.post('/api/users/:_id/exercises', saveExercise);
 
 export default router;
